0f9da7a142d675cfab76e828904bab476810d3e5
Julien <julien@ripouteau.com>
2022-09-18 15:06:25 +0200

    chore: test multiple database

--- tests-helpers/db.ts
+++ tests-helpers/db.ts
@@ -1,12 +1,22 @@
 import knex from 'knex'
+import type { Knex } from 'knex'
 
-export const connection = knex({
-  client: 'better-sqlite3',
-  useNullAsDefault: true,
-  connection: {
-    filename: './test.sqlite',
-  },
-})
+const credentials = {
+  host: 'localhost',
+  user: 'japa',
+  password: 'password',
+  database: 'japa',
+}
+
+const connectionConfig: { [key: string]: Knex.Config } = {
+  better_sqlite: { client: 'better-sqlite3', connection: { filename: ':memory:' } },
+  sqlite: { client: 'sqlite', connection: { filename: ':memory:' } },
+  mysql: { client: 'mysql2', connection: { ...credentials, port: 3306 } },
+  postgres: { client: 'pg', connection: { ...credentials, port: 5432 } },
+  mssql: { client: 'mssql', connection: { ...credentials, port: 1433 } },
+}
+
+export const connection = knex(connectionConfig[process.env.DB!]!)
 
 export const setupDb = async () => {
   await connection.schema.dropTableIfExists('user')
