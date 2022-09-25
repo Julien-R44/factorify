85860b229bdf5bb9d3862f762fde5ba8d360bcba
Julien <julien@ripouteau.com>
2022-09-25 17:03:07 +0200

    style: lint error

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,4 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 
-export default {
-  ...DefaultTheme,
-  enhanceApp({ app }) {
-    // register global components
-    // app.component('MyGlobalComponent', /* ... */)
-  }
-}
+export default DefaultTheme
